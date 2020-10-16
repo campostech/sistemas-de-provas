2c74f17a5e6c0c38bba528d8ac6ff9b22f588d06
Lucas Campos - CamposTech <53227451+campostech@users.noreply.github.com>
2020-10-16 18:57:03 -0300

    v1 solicitacoes css

--- solicitacoes.php
+++ solicitacoes.php
@@ -24,6 +24,10 @@ if($_SESSION['PERFIL'] != 1){
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/card.css">
+  <link rel="stylesheet" href="css/alerta.css">
 </head>
 <body class="hold-transition sidebar-mini text-sm accent-orange">
 <div class="wrapper">
